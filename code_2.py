8ff7ec89c551a614e75143d88436ce77c806a45f
Chad Collins <144498558+ChadCollins343@users.noreply.github.com>
2023-11-29 21:34:01 +0200

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,33 @@
+    });
+    const data = [];
+function generateRandomData() {
+}
+    }
+    for (let i = 0; i < 10; i++) {
+function main() {
+        console.log("Random Number: " + item);
+    return data;
+}
+
+    data.forEach(item => {
+
+main();
+        data.push(Math.floor(Math.random() * 100) + 1);
+    const data = generateRandomData();
+
+    });
+function generateRandomData() {
+
+    const data = [];
+}
+
+    for (let i = 0; i < 10; i++) {
+        console.log("Random Number: " + item);
+    data.forEach(item => {
+        data.push(Math.floor(Math.random() * 100) + 1);
+    }
+function main() {
+    return data;
+main();
+}
+    const data = generateRandomData();

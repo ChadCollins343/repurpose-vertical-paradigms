8793eea8cf537584fefe26fa3a0428d0bacc502f
Chad Collins <144498558+ChadCollins343@users.noreply.github.com>
2023-12-01 09:35:48 +0200

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,28 @@
+            data[i] = (int) (Math.random() * 100) + 1;
+        int[] data = new int[10];
+        for (int item : data) {
+public class RandomDataGenerator {
+}
+        for (int i = 0; i < 10; i++) {
+    public static void main(String[] args) {
+        }
+
+        }
+            System.out.println("Random Number: " + item);
+    }
+    const data = generateRandomData();
+main();
+    });
+    return data;
+        console.log("Random Number: " + item);
+    for (let i = 0; i < 10; i++) {
+}
+
+function generateRandomData() {
+function main() {
+        data.push(Math.floor(Math.random() * 100) + 1);
+    }
+    const data = [];
+    data.forEach(item => {
+
+}

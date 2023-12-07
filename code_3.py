db3d96b307b783e9618412bcdfb95ffbc548911f
Chad Collins <144498558+ChadCollins343@users.noreply.github.com>
2023-12-07 09:42:32 +0200

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,41 +1,52 @@
-}
+if __name__ == "__main__":
 
-function generateRandomData() {
-    return data;
-function main() {
 
-    const data = [];
-        data.push(Math.floor(Math.random() * 100) + 1);
 
-    }
-        console.log("Random Number: " + item);
-    data.forEach(item => {
-    for (let i = 0; i < 10; i++) {
-main();
-}
-    const data = generateRandomData();
-    });    }
-public class RandomDataGenerator {
-            System.out.println("Random Number: " + item);
-        for (int item : data) {
-            data[i] = (int) (Math.random() * 100) + 1;
 
-}
-        int[] data = new int[10];
-        for (int i = 0; i < 10; i++) {
-    public static void main(String[] args) {
-        }
-
-        }    for item in data:
-    return data
+    main()
 import random
-
+def main():
+    data = generate_random_data()
+    return data
+def generate_random_data():
+        print(f"Random Number: {item}")
+    data = [random.randint(1, 100) for _ in range(10)]
+    for item in data:    data = [random.randint(1, 100) for _ in range(10)]
+        print(f"Random Number: {item}")
+def generate_random_data():
+if __name__ == "__main__":
 
 def main():
     data = generate_random_data()
-    data = [random.randint(1, 100) for _ in range(10)]
+    for item in data:
 
-def generate_random_data():
+import random
+    return data
+
     main()
+    return data
+    data = [random.randint(1, 100) for _ in range(10)]
 if __name__ == "__main__":
+import random
+    main()
+def generate_random_data():
+
+    for item in data:
+def main():
+    data = generate_random_data()
+
         print(f"Random Number: {item}")
+
+            System.out.println("Random Number: " + item);
+            data[i] = (int) (Math.random() * 100) + 1;
+        }
+    public static void main(String[] args) {
+        int[] data = new int[10];
+        }
+        for (int item : data) {
+        for (int i = 0; i < 10; i++) {
+
+}
+
+    }
+public class RandomDataGenerator {

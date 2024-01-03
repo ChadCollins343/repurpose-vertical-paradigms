2574650d5a6dfcc33a26d905145e0410bd798c25
Chad Collins <144498558+ChadCollins343@users.noreply.github.com>
2024-01-03 10:13:08 +0200

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,17 +1,40 @@
-    const data = generateRandomData();
-    });
-function generateRandomData() {
-
-main();
-
-        data.push(Math.floor(Math.random() * 100) + 1);
+            data[i] = (int) (Math.random() * 100) + 1;
+    public static void main(String[] args) {
+        int[] data = new int[10];
+        for (int i = 0; i < 10; i++) {
 }
-}
-
-function main() {
-    const data = [];
+        }
+public class RandomDataGenerator {
+        for (int item : data) {
+            System.out.println("Random Number: " + item);
+        }
     }
+
         console.log("Random Number: " + item);
-    data.forEach(item => {
-    return data;
+}
+    }
+}
+
+    const data = [];
+main();
+    });
+function main() {
+    const data = generateRandomData();
+
+function generateRandomData() {
     for (let i = 0; i < 10; i++) {
+        data.push(Math.floor(Math.random() * 100) + 1);
+    return data;
+
+    data.forEach(item => {            System.out.println("Random Number: " + item);
+        }
+        int[] data = new int[10];
+}
+public class RandomDataGenerator {
+        for (int i = 0; i < 10; i++) {
+        for (int item : data) {
+    public static void main(String[] args) {
+
+            data[i] = (int) (Math.random() * 100) + 1;
+        }
+    }
